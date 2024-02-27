a2613564048386cb0c3b1b466badfeb3f31448c7
hellomurphy <hello.merphy@gmail.com>
2024-02-27 17:38:53 +0700

    update frontend

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,75 +1,258 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-      <v-img height="300" src="@/assets/logo.svg" />
+    <v-responsive class="align-center fill-height">
+      <div class="text-center">
+        <v-btn prepend-icon="mdi-check-circle">
+          <template v-slot:prepend>
+            <v-icon color="success"></v-icon>
+          </template>
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+          Import Excel
+        </v-btn>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+        <v-btn prepend-icon="mdi-check-circle">
+          <template v-slot:prepend>
+            <v-icon color="success"></v-icon>
+          </template>
 
-      <div class="py-14" />
+          Export Excel
+        </v-btn>
+      </div>
 
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
+      <v-data-table :headers="headers" :items="currentParts">
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title
+              >Master Parts changeover matrix </v-toolbar-title
+            >
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ props }">
+                <v-btn color="primary" dark class="mb-2" v-bind="props">
+                  New Part
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">{{ formTitle }}</span>
+                </v-card-title>
 
-            Components
-          </v-btn>
-        </v-col>
+                <v-card-text v-if="this.editedIndex === -1">
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="newParts"
+                          label="Current"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
 
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
+                <v-card-text v-else>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.current"
+                          label="Current"
+                        ></v-text-field>
+                      </v-col>
 
-            Get Started
-          </v-btn>
-        </v-col>
+                      <v-col
+                        v-for="part in parts"
+                        :key="part"
+                        cols="12"
+                        sm="6"
+                        md="4"
+                      >
+                        <v-text-field
+                          v-model="editedItem[part]"
+                          :label="part"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
 
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue-darken-1" variant="text" @click="close">
+                    Cancel
+                  </v-btn>
+                  <v-btn color="blue-darken-1" variant="text" @click="save">
+                    Save
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+              <v-card>
+                <v-card-title class="text-h5"
+                  >Are you sure you want to delete this item?</v-card-title
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="blue-darken-1"
+                    variant="text"
+                    @click="closeDelete"
+                    >Cancel</v-btn
+                  >
+                  <v-btn
+                    color="blue-darken-1"
+                    variant="text"
+                    @click="deleteItemConfirm"
+                    >OK</v-btn
+                  >
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon size="small" class="me-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        </template>
+      </v-data-table>
     </v-responsive>
   </v-container>
 </template>
 
-<script setup lang="ts">
-  //
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    newParts: "",
+    parts: ["A", "B", "C"],
+    headers: [],
+    currentParts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Part" : "Edit Part";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+    parts: {
+      handler(newVal, oldVal) {
+       this.initialize();
+      },
+      deep: true, 
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      const newParts = [];
+
+      this.parts.forEach((part) => {
+        const partObject = { current: part };
+        this.parts.forEach((p) => {
+          partObject[p] = 0;
+        });
+        newParts.push(partObject);
+      });
+
+      this.currentParts = newParts;
+
+      this.headers = [
+        {
+          title: "",
+          align: "start",
+          sortable: false,
+          key: "current",
+        },
+        ...this.parts.map((part) => {
+          return {
+            title: part,
+            key: part,
+          };
+        }),
+        {
+          title: "Edit Part",
+          key: "actions",
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.currentParts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.currentParts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.currentParts.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.currentParts[this.editedIndex], this.editedItem);
+      } else {
+        this.parts.push(this.newParts);
+      }
+      this.close();
+    },
+  },
+};
 </script>
