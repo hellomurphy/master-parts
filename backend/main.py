a1f2c6c01015f6a3b9e179dcd6ca20b11a326866
hellomurphy <hello.merphy@gmail.com>
2024-02-27 18:45:36 +0700

    add function export excel

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from openpyxl import Workbook
+from tempfile import NamedTemporaryFile
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+from typing import List, Union
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/generate_excel")
+async def generate_excel(data: List[List[Union[str, int]]]):
+    if not data:
+        raise HTTPException(status_code=400, detail="No data provided")
+
+    # Create a new Excel workbook
+    wb = Workbook()
+    ws = wb.active
+
+    # Add data to the workbook
+    for row in data:
+        ws.append(row)
+
+    # Save the workbook to a temporary file
+    with NamedTemporaryFile(delete=False) as tmp:
+        file_path = tmp.name
+        wb.save(file_path)
+
+        # Return the Excel file as a streaming response
+        return StreamingResponse(open(file_path, "rb"), media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,304 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <v-container class="fill-height">
+        <v-responsive class="align-center fill-height">
+          <div class="text-center">
+            <v-btn @click="importExcel" prepend-icon="mdi-check-circle">
+              <template v-slot:prepend>
+                <v-icon color="success"></v-icon>
+              </template>
+
+              Import Excel
+            </v-btn>
+
+            <v-btn @click="exportExcel" prepend-icon="mdi-check-circle">
+              <template v-slot:prepend>
+                <v-icon color="success"></v-icon>
+              </template>
+
+              Export Excel
+            </v-btn>
+          </div>
+
+          <v-data-table :headers="headers" :items="currentParts">
+            <template v-slot:top>
+              <v-toolbar flat>
+                <v-toolbar-title
+                  >Master Parts changeover matrix
+                </v-toolbar-title>
+                <v-divider class="mx-4" inset vertical></v-divider>
+                <v-spacer></v-spacer>
+                <v-dialog v-model="dialog" max-width="500px">
+                  <template v-slot:activator="{ props }">
+                    <v-btn color="primary" dark class="mb-2" v-bind="props">
+                      New Part
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="text-h5">{{ formTitle }}</span>
+                    </v-card-title>
+
+                    <v-card-text v-if="this.editedIndex === -1">
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              v-model="newParts"
+                              label="Current"
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+
+                    <v-card-text v-else>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              v-model="editedItem.current"
+                              label="Current"
+                            ></v-text-field>
+                          </v-col>
+
+                          <v-col
+                            v-for="part in parts"
+                            :key="part"
+                            cols="12"
+                            sm="6"
+                            md="4"
+                          >
+                            <v-text-field
+                              v-model="editedItem[part]"
+                              :label="part"
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        color="blue-darken-1"
+                        variant="text"
+                        @click="close"
+                      >
+                        Cancel
+                      </v-btn>
+                      <v-btn color="blue-darken-1" variant="text" @click="save">
+                        Save
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+                <v-dialog v-model="dialogDelete" max-width="500px">
+                  <v-card>
+                    <v-card-title class="text-h5"
+                      >Are you sure you want to delete this item?</v-card-title
+                    >
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        color="blue-darken-1"
+                        variant="text"
+                        @click="closeDelete"
+                        >Cancel</v-btn
+                      >
+                      <v-btn
+                        color="blue-darken-1"
+                        variant="text"
+                        @click="deleteItemConfirm"
+                        >OK</v-btn
+                      >
+                      <v-spacer></v-spacer>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-toolbar>
+            </template>
+            <template v-slot:item.actions="{ item }">
+              <v-icon size="small" class="me-2" @click="editItem(item)">
+                mdi-pencil
+              </v-icon>
+              <v-icon size="small" @click="deleteItem(item)">
+                mdi-delete
+              </v-icon>
+            </template>
+            <template v-slot:no-data>
+              <v-btn color="primary" @click="initialize"> Reset </v-btn>
+            </template>
+          </v-data-table>
+        </v-responsive>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
-<script setup lang="ts">
-  //
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    newParts: "",
+    parts: ["A", "B", "C"],
+    headers: [],
+    currentParts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Part" : "Edit Part";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+    parts: {
+      handler(newVal, oldVal) {
+        this.initialize();
+      },
+      deep: true,
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      const newParts = [];
+
+      this.parts.forEach((part) => {
+        const partObject = { current: part };
+        this.parts.forEach((p) => {
+          partObject[p] = 0;
+        });
+        newParts.push(partObject);
+      });
+
+      this.currentParts = newParts;
+
+      this.headers = [
+        {
+          title: "",
+          align: "start",
+          sortable: false,
+          key: "current",
+        },
+        ...this.parts.map((part) => {
+          return {
+            title: part,
+            key: part,
+          };
+        }),
+        {
+          title: "Edit Part",
+          key: "actions",
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.currentParts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.currentParts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.currentParts.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.currentParts[this.editedIndex], this.editedItem);
+      } else {
+        this.parts.push(this.newParts);
+      }
+      this.close();
+    },
+
+    generateResult(headers, currentParts) {
+      const dataToSend = [headers.slice(0, -1).map((header) => header.title)];
+
+      currentParts.forEach((part) => {
+        const rowData = [part.current];
+        rowData.push(part.A, part.B, part.C);
+        dataToSend.push(rowData);
+      });
+
+      return dataToSend;
+    },
+
+    async importExcel() {},
+
+    async exportExcel() {
+      const dataToSend = this.generateResult(this.headers, this.currentParts);
+      const URL = "http://127.0.0.1:8000";
+
+      try {
+
+        const response = await axios.post(`${URL}/generate_excel`, dataToSend, {
+          responseType: "blob",
+        });
+
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "data.xlsx");
+        document.body.appendChild(link);
+        link.click();
+      } catch (err) {
+        console.error("Error generating Excel file:", err);
+      }
+    },
+  },
+};
 </script>
